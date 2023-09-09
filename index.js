90fa6aea1199d2ccf7f6f03da57696e07f8af782
Talha-G <mtalhagujjar285@gmail.com>
2023-09-09 18:29:16 +0500

    path changed

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const studentsRoutes = require('./routes/students')
+const mongoose = require('mongoose')
+
+
+
+app.use(express.json())
+app.use('/api/students',studentsRoutes)
+
+
+
+mongoose.connect('mongodb+srv://mtalhagujjar285:mt@lh@gujj@r@talha-cluster.wp71qfl.mongodb.net/talha?retryWrites=true&w=majority').then((res)=>{
+    console.log("Connected!")
+}).catch((err)=>{
+    console.log(err.message)
+})
+
+
+
+var port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log("server is running")
+})
